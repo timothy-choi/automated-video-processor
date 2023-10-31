70b532c1a1522285107024718e679845726a172f
timothy-choi <tchoi720@gmail.com>
2023-10-31 16:50:21 -0700

    adding first schema of Accounts

---
+++ Server/API/Accounts/Accounts.java
@@ -0,0 +1,73 @@
+package api.accounts;
+
+import javax.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.Date;  
+
+
+@Document(collection = "accounts")
+public class Accounts {
+    @Id
+    private @GeneratedValue Long accountId;
+    private String name;
+    private String username;
+
+    private Date joined;
+    private String email;
+
+    private Int NumberOfVideosMade;
+    private List<Long> VideosCreated;
+    
+    Accounts() {}
+
+    Accounts(String name, String username, String email) {
+        this.accountId = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        this.name = name;
+        this.username = username;
+        Date date = new Date();
+        this.joined = date;
+        this.email = email;
+        this.NumberOfVideosMade = 0;
+        VideosCreated = new ArrayList<Long>();
+    }
+
+    public Long getId() {
+        return this.accountId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public Date getJoinedDate() {
+        return this.joined;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setNumVideos(int change) {
+        this.NumberOfVideosMade += change;
+    }
+
+    public int getNumVideos() {
+        return this.NumberOfVideosMade;
+    }
+
+    public void addVideo(Long videoId) {
+        VideosCreated.add(videoId);
+    }
+
+    public List<Long> getVideos() {
+        return VideosCreated;
+    }
+}
