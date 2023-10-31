9d617fc0e5f30067b5968ecb386ec93b109dc555
timothy-choi <tchoi720@gmail.com>
2023-10-31 13:47:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# automated-video-processor
---
+++ Server/drive/src/test/java/com/example/drive/DriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
