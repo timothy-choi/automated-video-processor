698ba169819703d77cceed49afc118b6f3714cf4
timothy-choi <tchoi720@gmail.com>
2023-11-01 15:09:21 -0700

    adding creating a new RestTemplate instance in driver

--- Server/drive/src/main/java/com/example/drive/DriveApplication.java
+++ Server/drive/src/main/java/com/example/drive/DriveApplication.java
@@ -2,12 +2,19 @@ package com.example.drive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DriveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DriveApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
