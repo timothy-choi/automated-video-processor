07c72d7000a088554137e9f2f7234725ba55b0a6
timothy-choi <tchoi720@gmail.com>
2023-11-14 13:04:55 -0800

    adding delete video endpoint

--- Server/API/VideoAccounts/VideoAccountsController.java
+++ Server/API/VideoAccounts/VideoAccountsController.java
@@ -1,5 +1,7 @@
 package api.VideoAccounts;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import api.VideoAccounts.PrimaryKey;
 import api.videoAccounts.VideoAccounts;
 import api.videoProcessing.VideoAccountsRepository;
+import api.WebClientConfig;
 
 @RestController
 public class VideoAccountsController {
@@ -47,15 +50,33 @@ public class VideoAccountsController {
     @PostMapping("/videoAccounts")
     public ResponseEntity createVideoAccount(@RequestBody Map reqInfo) {
         try {
-            VideoAccounts acct = new VideoAccounts()
-                .setUserId()
-                .setUsername(reqInfo.get("username"))
-                .createMadeVideos()
-                .setVideoBucket(reqInfo.get("bucketname"));
-            
+            VideoAccounts acct = new VideoAccounts();
             acct.setVideoAccountId();
+            acct.setUserId();
+            acct.setUsername(reqInfo.get("username"));
+            acct.createMadeVideos();
+            acct.setVideoBucket(reqInfo.get("bucketname"));
 
             _videoAccountsRepository.save(acct);
+            return ResponseEntity.ok();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/videoAccounts/{userId}/{videoAccountId}/{video}")
+    public ResponseEntity deleteVideo(@PathVariable("userId") String userId, @PathVariable("videoAccountId") String videoAccountId, @PathVariable("video") String video) {
+        try {
+            VideoAccounts videoAcct = WebClientConfig.WebClient().get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/videoAccounts/{userId}/{videoAccountId}/{publicDisplay}")
+                .build(reqInfo.get("userId"), reqInfo.get("videoAccountId"), false))
+            .retrieve()
+            .bodyToMono(VideoAccounts.class);
+
+            videoAcct.delete(video);
+            _videoAccountsRepository.save(videoAcct);
+
             return ResponseEntity.ok();
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
@@ -65,13 +86,14 @@ public class VideoAccountsController {
     @DeleteMapping("/videoAccounts/{userId}/{videoAccountId}")
     public ResponseEntity deleteVideoAccount(@PathVariable("userId") String userId, @PathVariable("videoAccountId") String videoAccountId) {
         try {
-            PrimaryKey currKey = new PrimaryKey();
-            currKey.setUserId(userId);
-            currKey.setVideoAccountId(videoAccountId);
+            VideoAccounts videoAcct = WebClientConfig.WebClient().get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/videoAccounts/{userId}/{videoAccountId}/{publicDisplay}")
+                .build(userId, videoAccountId, false))
+            .retrieve()
+            .bodyToMono(VideoAccounts.class);
 
-            VideoAccounts acct = _videoAccountsRepository.findById(currKey);
-
-            _videoAccountsRepository.delete(acct);
+            _videoAccountsRepository.delete(videoAcct);
 
             return ResponseEntity.ok();
         } catch (Exception e) {
