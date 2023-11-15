a352b03ad3211a8d665f3fdfbd042bd80f242ee3
timothy-choi <tchoi720@gmail.com>
2023-11-15 12:49:09 -0800

    adding new token request helper function

--- Server/GCP/UserAuthFlow.java
+++ Server/GCP/UserAuthFlow.java
@@ -4,10 +4,16 @@ package api.gcp;
 import java.util.*;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.TokenResponse;
 
 public class UserAuthFlow {
     public static String getAuthUrl(AuthorizationCodeFlow flow) {
         AuthorizationCodeRequestUrl authUrl = flow.newAuthorizationUrl();
-        return authUrl.setRedirectUrl("").build(); 
+        return authUrl.setRedirectUri("").build(); 
+    }
+
+    public static TokenResponse sendTokenRequest(String code, AuthorizationCodeFlow flow, List<String> scopes) {
+        TokenResponse res = flow.newTokenRequest(code).setScopes(scopes).setRedirectUri("").build();
+        return res;
     }
 }
