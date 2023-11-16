b53f6b5e9644c2b0e65f8726e6d577a67ef44ad4
timothy-choi <tchoi720@gmail.com>
2023-11-16 00:52:08 -0800

    adding helper func to create video data

--- Server/Youtube/YoutubeHelper.java
+++ Server/Youtube/YoutubeHelper.java
@@ -5,8 +5,14 @@ import com.google.api.client.auth.oauth2.Credential;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
+
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoSnippet;
+import com.google.api.services.youtube.model.VideoStatus;
 
 
 public class YoutubeHelper {
@@ -16,4 +22,20 @@ public class YoutubeHelper {
             .setApplicationName("Video Processor")
             .build();
     } 
+
+    public static Video createVideo(String title, String desc, String privacy, List<String> tags) {
+        Video videoMetaData = new Video();
+        VideoSnippet snippets = new VideoSnippet();
+        snippets.setTitle(title);
+        snippets.setDescription(desc);
+        snippets.setTags(tags);
+
+        VideoStatus status = new VideoStatus();
+        status.setPrivacyStatus(privacy);
+        videoMetaData.setStatus(status);
+
+        videoMetaData.setSnippets(snippets);
+
+        return videoMetaData;
+    }
 }
