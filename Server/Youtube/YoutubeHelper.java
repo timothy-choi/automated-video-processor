269c2257cf6fe5085496a2a51305d96692737cdc
timothy-choi <tchoi720@gmail.com>
2023-11-17 01:01:12 -0800

    adding helper functions to upload and conntinue video upload if network connection issues exist

--- Server/Youtube/YoutubeHelper.java
+++ Server/Youtube/YoutubeHelper.java
@@ -17,6 +17,8 @@ import com.google.api.services.youtube.model.VideoSnippet;
 import com.google.api.services.youtube.model.VideoStatus;
 
 import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import java.util.Random;
 
 public class YoutubeHelper {
     public static YouTube getService(Credential userCredentials) throws IOException, GeneralSecurityException {
@@ -42,7 +44,7 @@ public class YoutubeHelper {
         return videoMetaData;
     }
 
-    public static Video uploadVideo(Video selectedVideo, String filename, YouTube service) {
+    public static Insert uploadVideo(Video selectedVideo, String filename, YouTube service) {
         File videoFile = new File(filename);
         InputStreamContent mediaContent = new InputStreamContent("application/octet-stream", new BufferedInputStream(new FileInputStream(videoFile)));
 
@@ -50,6 +52,55 @@ public class YoutubeHelper {
 
         Video uploadedVideo = insertVideo.execute();
 
-        return uploadedVideo;
+        return insertVideo;
+    }
+
+    private static bool checkError(IOException e) {
+        if (e instanceof GoogleJsonResponseException) {
+            int code = ((GoogleJsonResponseExceptionle) e).getStatusCode();
+            if (code == 500 || code == 502 || code == 503 || code == 504) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String resumableUpload(Video videoReq) {
+        String error = "";
+        Video response = null;
+        int retry = 0;
+
+        while (response == null) {
+            try {
+                Video vidContent = videoReq.execute();
+
+                if (vidContent != null) {
+                    response = vidContent;
+                }
+                else {
+                    throw new Exception("Error processing video");
+                }
+            } catch (IOException e) {
+                if (checkError(e)) {
+                    error = "res error";
+                }
+                else {
+                    throw new Exception("Error processing video");
+                }
+            }
+
+            if ("".equals(error) != true) {
+                retry++;
+                if (retry > 3) {
+                    throw new Exception("Can't upload video");
+                }
+
+                int sleepDurations = new Random().nextInt((int) Math.pow(2, retry)) + 1;
+                Thread.sleep(sleepDurations);
+            } 
+            
+        }
+
+        return response.getId();
     }
 }
